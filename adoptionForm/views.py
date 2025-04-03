a49689fdc95799ff3952b5897c41c28b2451612b
harukanon <harukanon214@icloud.com>
2025-04-03 22:46:07 +0800

    django meets html

--- adoptionForm/views.py
+++ adoptionForm/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the adoptionForm index.")
+
+def adoptionForm(request):
+    return render(request, 'adoptionForm/adoptionForm.html')
