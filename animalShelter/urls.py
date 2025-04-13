cd15d25331a25c945c88b9c24b710ffe07a15d14
harukanon <harukanon214@icloud.com>
2025-04-13 19:24:12 +0800

    project django url pattern change

--- animalShelter/urls.py
+++ animalShelter/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-        path("adoptionForm/", include("adoptionForm.urls")),
+    path("adoptionForm/", include("adoptionForm.urls")),
     path('admin/', admin.site.urls),
 ]
